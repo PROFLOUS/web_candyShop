00bb73700744078ac9ed92753be819ed01e97ffd
tandang <cuctrau159@gmai.com>
2022-05-04 00:19:03 +0700

    first commit

---
+++ src/main/java/com/example/webcandy/web_candy/WebCandyApplication.java
@@ -0,0 +1,13 @@
+package com.example.webcandy.web_candy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCandyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCandyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webcandy/web_candy/WebCandyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webcandy.web_candy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCandyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
